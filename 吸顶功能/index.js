c64cd4eb322f35a365960b36c61d045167987e39
yunlangjy <2403965413@qq.com>
2024-07-03 13:48:45 +0800

    bug

--- 吸顶功能/index.js
+++ 吸顶功能/index.js
@@ -1,29 +1,50 @@
 /**
- * 元素滚动到某位置有吸顶效果
+ * 吸顶效果
+ * @param {*} containerElement 能滚动的元素
+ * @param {*} placeholderElement 吸顶元素的父元素（占位元素）
+ * @param {*} fixedElement 吸顶元素
+ * @param {*} positionTop 吸顶距离，头部高度
+ * @returns 
  */
-function fixedTop() {
-    const contentEl = document.getElementById('content') // 能滚动的元素
-    const fixedEl = document.getElementsByClassName('scroll-to-fixed-top')[0] // 吸顶元素
-    const headerHeight = 54 // 吸顶距离，头部高度
+function scrollFixed(containerElement, placeholderElement, fixedElement, positionTop) {
 
-    const contentLeft = contentEl.offsetLeft
+    if (!(containerElement instanceof HTMLElement)) {
+        console.error(new TypeError('scrollFixed函数参数错误，第1个参数应为HTML元素').message)
+        return
+    }
+
+    if (!(placeholderElement instanceof HTMLElement)) {
+        console.error(new TypeError('scrollFixed函数参数错误，第2个参数应为HTML元素').message)
+        return
+    }
+
+    if (!(fixedElement instanceof HTMLElement)) {
+        console.error(new TypeError('scrollFixed函数参数错误，第3个参数应为HTML元素').message)
+        return
+    }
+
+    if (typeof positionTop !== 'number') {
+        console.error(new TypeError('scrollFixed函数参数错误，第4个参数应为数字').message)
+        return
+    }
+
+    const contentLeft = containerElement.offsetLeft
     const styleEl = document.createElement('style');
     styleEl.type = 'text/css';
-    styleEl.innerHTML = `.xx-scroll-fixed {left: ${contentLeft}px; width: calc(100vw - ${contentLeft}px); position: fixed; box-sizing: border-box; top: ${headerHeight}px;z-index: 555; box-shadow: 0 10px 10px -10px rgba(0, 0, 0, 0.1);}`
+    styleEl.innerHTML = `.xx-scroll-fixed {left: ${contentLeft}px; width: calc(100vw - ${contentLeft}px); position: fixed; box-sizing: border-box; top: ${positionTop}px;z-index: 555; box-shadow: 0 10px 10px -10px rgba(0, 0, 0, 0.1);}`
     document.head.appendChild(styleEl);
 
-    const startPosition = fixedEl.getBoundingClientRect().top
-    contentEl.onscroll = function() {
-        const fixedElTop = fixedEl.getBoundingClientRect().top
-        const contentElTop = contentEl.scrollTop
-        if (!fixedEl.classList.contains('xx-scroll-fixed')) {
-        if (fixedElTop <= headerHeight) {
-            fixedEl.classList.add('xx-scroll-fixed')
-        }
-        } else if (contentEl.scrollTop < startPosition + 5) {
-        fixedEl.classList.remove('xx-scroll-fixed')
+    placeholderElement.style.height = `${fixedElement.getBoundingClientRect().height}px`
+
+    const startPosition = fixedElement.getBoundingClientRect().top
+    containerElement.onscroll = function() {
+        const fixedElTop = fixedElement.getBoundingClientRect().top
+        if (!fixedElement.classList.contains('xx-scroll-fixed')) {
+            if (fixedElTop <= positionTop) {
+                fixedElement.classList.add('xx-scroll-fixed')
+            }
+        } else if (containerElement.scrollTop < startPosition + 5) {
+            fixedElement.classList.remove('xx-scroll-fixed')
         }
     }
 }
-
-window.onload = fixedTop
